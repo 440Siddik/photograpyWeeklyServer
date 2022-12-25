ac288d2db3195fec384770ec4f175e35afd8a040
Afraaz <ahmedalif102@gmail.com>
2022-12-25 22:20:27 +0600

    all api done

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
-
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h9ef9oj.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,33 +18,91 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-async function run () {
-try{
-const serviceCollection = client.db('phWeekly').collection('services')
+async function run() {
+  try {
+    const serviceCollection = client.db("phWeekly").collection("services");
 
-app.get('/services', async(req, res) => {
-  const query = {}
-  const cursor = serviceCollection.find(query).limit(3);
-  const services = await cursor.toArray();
-  res.send(services)
-})
+    const reviewsCollection = client.db("phWeekly").collection("reviews");
 
-app.get('/allservices', async(req, res) => {
-  const query = {};
-  const cursor = serviceCollection.find(query)
-  const allservices = await cursor.toArray()
-  res.send(allservices)
-})
+    const addServiceCollection = client.db("phWeekly").collection("addservice");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/allservices", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const allservices = await cursor.toArray();
+      res.send(allservices);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //reviews
+
+    app.get("/myreviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.get("/allreviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const allreviews = await cursor.toArray();
+      res.send(allreviews);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const reviews = req.body;
+      const result = await reviewsCollection.insertOne(reviews);
+      res.send(result);
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // addservice api
+    app.post("/addservice", async (req, res) => {
+      const addedService = req.body;
+      const result = await addServiceCollection.insertOne(addedService);
+      res.send(result);
+    });
+
+    app.get("/addservice", async (req, res) => {
+      const query = {};
+      const cursor = addServiceCollection.find(query);
+      const addServices = await cursor.toArray();
+      res.send(addServices);
+    });
+  } finally {
+  }
 }
-finally{}
-}
-run().catch(err => console.log(err))
+run().catch((err) => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.get('/', (req, res) => {
-res.send('Server is running')
-})
-
-app.listen(port , (req, res) => {
+app.listen(port, (req, res) => {
   console.log(`Server is running on port ${port}`);
-})
+});
